4d31846b7a20d1d782119d8f2c69487f67a93db6
rifki achmad fachrezy <rifkiachmadfachrezy@gmail.com>
2026-03-26 22:10:07 +0700

    update route baseurl

--- app/api/blast/route.ts
+++ app/api/blast/route.ts
@@ -13,24 +13,29 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "Guest not found" }, { status: 404 });
     }
 
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000";
+    const baseUrl =
+      process.env.NEXT_PUBLIC_BASE_URL ??
+      (process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : `${req.headers.get("x-forwarded-proto") ?? "http"}://${req.headers.get("host")}`);
+
     const invitationUrl = `${baseUrl}/invitation/${guest.slug}`;
 
     const message =
-    `*Assalamu'alaikum Wr. Wb.*\n\n` +
-    `Kepada Yth.\n*${guest.name}*\n\n` +
-    `_Kami percaya bahwa setiap pertemuan adalah takdir, dan setiap pasangan adalah amanah. Dengan ridha-Nya, kami memulai perjalanan ini, dalam ikatan suci yang penuh keberkahan.🕊️_\n\n` +
-    `Dengan memohon rahmat dan ridho Allah Subhanahuwata'ala, kami bermaksud mengundang *${guest.name}* untuk menghadiri acara pernikahan kami.\n\n` +
-    `*Berikut link undangan digital untuk ${guest.name}:*\n` +
-    `${invitationUrl}\n\n` +
-    `Merupakan suatu kehormatan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu.\n\n` +
-    `*Wassalamu'alaikum Wr. Wb.*\n` +
-    `Rifki & Ayu 💍`;
+      `*Assalamu'alaikum Wr. Wb.*\n\n` +
+      `Kepada Yth.\n*${guest.name}*\n\n` +
+      `_Kami percaya bahwa setiap pertemuan adalah takdir, dan setiap pasangan adalah amanah. Dengan ridha-Nya, kami memulai perjalanan ini, dalam ikatan suci yang penuh keberkahan.🕊️_\n\n` +
+      `Dengan memohon rahmat dan ridho Allah Subhanahuwata'ala, kami bermaksud mengundang *${guest.name}* untuk menghadiri acara pernikahan kami.\n\n` +
+      `*Berikut link undangan digital untuk ${guest.name}:*\n` +
+      `${invitationUrl}\n\n` +
+      `Merupakan suatu kehormatan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir dan memberikan doa restu.\n\n` +
+      `*Wassalamu'alaikum Wr. Wb.*\n` +
+      `Rifki & Ayu 💍`;
 
     const phone = guest.phone
       .replace(/\D/g, "")
       .replace(/^0/, "62");
-      console.log("TOKEN:", process.env.FONNTE_TOKEN);
+
     const res = await fetch("https://api.fonnte.com/send", {
       method: "POST",
       headers: {
